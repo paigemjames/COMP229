7a8fcdb569106e453befafa5a235a6738509577e
paigemjames <paigemjames98@gmail.com>
2023-09-19 19:11:16 -0400

    will this work

---
+++ week_1/modules/main.js
@@ -0,0 +1,12 @@
+import {half0f, multiply} from './lib.js';
+console.log(half0f(84));
+console.log(multiply(21,2));
+
+import doSomething from './doSomething.js';
+doSomething();
+
+import { flag, touch } from 'validator';
+console.log(flag);
+touch();
+console.log(flag);
+
